2ffb5081047389cf9a85d1afe98a3585be6afc56
michaelbennie <michaelbennie@ufl.edu>
2024-10-31 01:59:18 -0400

    added judgelm into another direcotry

--- advercialModel/judgelm/llm_judge/gen_model_judgement_multi.py
+++ advercialModel/judgelm/llm_judge/gen_model_judgement_multi.py
@@ -17,9 +17,10 @@ root = file.parents[2]
 sys.path.append(str(root))
 print(sys.path)
 
-from judgelm.llm_judge.common import load_questions, reorg_answer_file, conv_judge_pair, conv_judge_pair_w_reference, KeywordsStoppingCriteria, parse_score, translate_score_to_win_list
+from judgelm.llm_judge.common import load_questions, reorg_answer_file, conv_judge_multi, conv_judge_multi_w_reference, KeywordsStoppingCriteria, parse_score, translate_score_to_win_list
 from judgelm.model import load_model
 from judgelm.utils import extract_jsonl
+
 
 def run_eval(
     model_path,
@@ -35,7 +36,8 @@ def run_eval(
     temperature,
     if_reverse_answers,
     reference_file,
-    if_fast_eval
+    if_fast_eval,
+    answer_num
 ):
     print("start run_eval")
     questions = load_questions(question_file, question_begin, question_end)
@@ -70,6 +72,7 @@ def run_eval(
                 if_reverse_answers,
                 references[i : i + chunk_size] if reference_file is not None else None,
                 if_fast_eval,
+                answer_num
             )
         )
 
@@ -90,6 +93,7 @@ def get_model_answers(
     if_reverse_answers,
     references,
     if_fast_eval,
+    answer_num
 ):
     print("start load model")
     model, tokenizer = load_model(
@@ -97,27 +101,14 @@ def get_model_answers(
         device="cuda",
         num_gpus=num_gpus_per_model,
         max_gpu_memory=max_gpu_memory,
-        load_8bit=True,
-        cpu_offloading=True,
+        load_8bit=False,
+        cpu_offloading=False,
         debug=False,
     )
 
-    print(f"Parameters of get_model_answers:\n"
-          f"  model_path: {model_path}\n"
-          f"  model_id: {model_id}\n"
-          f"  questions: {questions}\n"
-          f"  answer_file: {answer_file}\n"
-          f"  max_new_token: {max_new_token}\n"
-          f"  num_gpus_per_model: {num_gpus_per_model}\n"
-          f"  max_gpu_memory: {max_gpu_memory}\n"
-          f"  temperature: {temperature}\n"
-          f"  if_reverse_answers: {if_reverse_answers}\n"
-          f"  references: {references}\n"
-          f"  if_fast_eval: {if_fast_eval}\n")
-
     for q_i, question in tqdm(enumerate(questions)):
         torch.manual_seed(q_i)
-        conv = conv_judge_pair.copy(None) if references is None else conv_judge_pair_w_reference.copy(None) #HAU ALDATU DOT DANDOLE UN DEFAULT VALUE VACIO EN LOS DOS CASOS SINO ME DECIA QUE MISSING POSSITIONAL AL USAR EL DE SOLO DOS
+        conv = conv_judge_multi.copy(answer_num) if references is None else conv_judge_multi_w_reference.copy(answer_num)
         template = conv.prompt_template
 
         # if fast eval, use the "\n" as the separator
@@ -130,17 +121,23 @@ def get_model_answers(
             question["answer1_body"] = question["answer2_body"]
             question["answer2_body"] = temp_answer
 
+        answer_more = ""
+        for i in range(answer_num - 2):
+            answer_more += conv.answer_format.format(answer_id=i+3, answer=question[f'answer{i+3}_body'])
+
         # combine data_sample
         if references is None:
             data_sample = conv.system + '\n' + template.format(question=question['question_body'],
                                                                answer_1=question['answer1_body'],
                                                                answer_2=question['answer2_body'],
+                                                               answer_more=answer_more,
                                                                prompt=conv.prompt) + conv.appendix
         else:
             data_sample = conv.system + '\n' + template.format(question=question['question_body'],
                                                                reference=references[q_i]['reference']['text'],
                                                                answer_1=question['answer1_body'],
                                                                answer_2=question['answer2_body'],
+                                                               answer_more=answer_more,
                                                                prompt=conv.prompt) + conv.appendix
 
         input_ids = tokenizer([data_sample]).input_ids
@@ -256,6 +253,12 @@ if __name__ == "__main__":
         default=0,
         help="Whether to use fast evaluation.",
     )
+    parser.add_argument(
+        "--answer-num", 
+        type=int,
+        default=2,
+        help="The number of answers."
+    )
     args = parser.parse_args()
     args.if_reverse_answers = bool(args.if_reverse_answers)
     args.if_fast_eval = bool(args.if_fast_eval)
@@ -285,28 +288,8 @@ if __name__ == "__main__":
         args.temperature,
         args.if_reverse_answers,
         args.reference_file,
-        args.if_fast_eval
+        args.if_fast_eval,
+        args.answer_num
     )
-    
+
     reorg_answer_file(args.answer_file)
-
-    # statistics the judgements
-    sequential_pred_answer_file_list = extract_jsonl(args.answer_file)
-
-    sequential_pred_score_list = []
-    for sequential_pred_answer_file in sequential_pred_answer_file_list:
-        sequential_pred_score_list.append(parse_score(sequential_pred_answer_file['pred_text']))
-
-    # if the score gap is less than T, we consider it as a draw
-    T = 0.0
-    sequential_pred_win_list = translate_score_to_win_list(sequential_pred_score_list, T)
-
-    # get the number of 1 in sequential_pred_win_list
-    win_num = sequential_pred_win_list.count(1)
-    tie_num = sequential_pred_win_list.count(0)
-    lose_num = sequential_pred_win_list.count(-1)
-
-    # print the win, tie, and lose number, use format {}
-    print("Assistant 1's reuslts ---> win_num: {}, tie_num: {}, lose_num: {}".format(win_num, tie_num, lose_num))
-    print("Assistant 2's reuslts ---> win_num: {}, tie_num: {}, lose_num: {}".format(lose_num, tie_num, win_num))
-
